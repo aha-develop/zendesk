46ce66c281af464f56e2be84007da1adb13d1955
Percy Hanna <percy@aha.io>
2021-05-04 13:24:52 -0600

    Update importer hooks
    - Use new auth pattern for importers
    - Standardize description for new record
    - Set ticket field on import, so it also shows up on full page view

--- src/import.js
+++ src/import.js
@@ -1,7 +1,30 @@
-import { settings } from "./extension";
-import { zendeskFetch } from "./zendesk";
+import { EXTENSION_ID, TICKET_FIELD, settings } from "./extension";
+import { zendeskFetch, descriptionForItem } from "./zendesk";
 
 const importer = aha.getImporter("aha-develop.zendesk.import");
+
+// Return the available filters
+importer.on(
+  {
+    action: "listFilters",
+  },
+  async () => {
+    // Trigger API request to force authentication
+    await zendeskFetch("/users/me");
+
+    return { view: { title: "View", required: true, type: "select" } };
+  },
+);
+
+// For a particular filter, when it is dropped-down, provide a list of the possible values.
+importer.on({ action: "filterValues" }, async ({ filterName, filters }) => {
+  switch (filterName) {
+    case "view": {
+      const response = await zendeskFetch("/views");
+      return response.views.filter(view => view.active).map(view => ({ value: view.id, text: view.title }));
+    }
+  }
+});
 
 // Return an array of records from a paginated list of import candidates.
 importer.on({ action: "listCandidates" }, async ({ filters, nextPage }) => {
@@ -17,27 +40,16 @@ importer.on({ action: "listCandidates" }, async ({ filters, nextPage }) => {
       identifier: item.ticket.id,
       name: item.subject,
       url: `https://${settings.subdomain}.zendesk.com/agent/tickets/${item.ticket.id}`,
+      description: descriptionForItem(item),
     })),
     nextPage: null,
   };
 });
 
-// Return the available filters
-importer.on(
-  {
-    action: "listFilters",
-  },
-  async () => {
-    return { view: { title: "View", required: true, type: "select" } };
-  },
-);
+importer.on({ action: "importRecord" }, async ({ importRecord, ahaRecord }) => {
+  // Save reference to ticket for the full page screen
+  ahaRecord.setExtensionField(EXTENSION_ID, TICKET_FIELD, importRecord.identifier);
 
-// For a particular filter, when it is dropped-down, provide a list of the possible values.
-importer.on({ action: "filterValues" }, async ({ filter, filters }) => {
-  switch (filter) {
-    case "view": {
-      const response = await zendeskFetch("/views");
-      return response.views.filter(view => view.active).map(view => ({ id: view.id, name: view.title }));
-    }
-  }
+  ahaRecord.description = importRecord.description;
+  ahaRecord.save();
 });
