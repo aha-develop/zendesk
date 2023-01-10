73e2d63114ba7461b904882b8406d1423e540b09
louisfnz <25600218+louisfnz@users.noreply.github.com>
2023-01-11 12:26:12 +1300

    Trim whitespace from subdomain

--- src/extension.js
+++ src/extension.js
@@ -1,5 +1,10 @@
 export const EXTENSION_ID = "aha-develop.zendesk";
-export const settings = aha.settings.get(EXTENSION_ID);
+
+const baseSettings = aha.settings.get(EXTENSION_ID);
+export const settings = {
+  ...baseSettings,
+  subdomain: (baseSettings.subdomain ?? "").trim(),
+};
 
 // Fields used on records
 export const TICKET_FIELD = "zendesk.ticket";
