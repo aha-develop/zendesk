2aed57be9f5181e31395ec9bd358a460d0d4fffe
Tim Field <tim@mohiohio.com>
2026-04-20 11:24:27 +1200

    Explanation

--- src/lib/useViewData.ts
+++ src/lib/useViewData.ts
@@ -3,17 +3,23 @@ import { loadViewData, sharedStore } from "../store";
 import { View, ViewData, ZendeskItem } from "../types";
 import { idToData } from "../tickets/columnFormatter";
 
+/**
+ * Load data ( tickets ) for a given view definition.
+ * Also applies client side filtering based on the search term.
+ */
 export function useViewData(view: View): { loading: boolean; viewData: ViewData | null } {
   const { loading, data: viewData } = sharedStore.viewData?.[view.id] ?? { loading: false, data: null };
   const fetchData = !!viewData && !loading;
   const searchTerm = useDeferredValue(sharedStore.searchTerm);
 
+  // Load data for the view if we don't have it already
   useEffect(() => {
     if (!fetchData) {
       loadViewData(view.id);
     }
   }, [fetchData, view.id]);
 
+  // Apply client side filtering based on the search term
   const filteredViewData: ViewData | null = useMemo(() => {
     if (!viewData?.rows) return viewData;
 
@@ -21,6 +27,7 @@ export function useViewData(view: View): { loading: boolean; viewData: ViewData 
 
     const searchValue = searchTerm.toLowerCase();
     const columnsRender = Object.fromEntries(
+      // Search the formatted column values, itToData is used to render the cells
       viewData.columns.map(column => [column.id, idToData(column.id, viewData)] as const),
     );
 
